aba49a1dac4668a37475da9d9ee20a072cfd4604
Nellie Allsop <nellie.allsop@gmail.com>
2023-11-27 12:39:16 +0000

    end of demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from 'next/image'
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
-  return (
-    <main>
- <p>You are looking at my root route!</p>
-    </main>
-  )
+export default async function Home() {
+	await sql`INSERT INTO ViewsTable (views) SELECT 0 WHERE NOT EXISTS (SELECT * FROM ViewsTable)`;
+	await sql`UPDATE ViewsTable SET views = views + 1`;
+	const result = await sql`SELECT views from ViewsTable`;
+
+	let time = await fetch("http://worldtimeapi.org/api/timezone/Europe/London", { next: {revalidate: 10}});
+  
+	const data = await time.json();
+	const datetime = new Date(data.datetime);
+	const evenEasierToReadDate = datetime.toLocaleTimeString("en-GB");
+
+	// console.log(data)
+
+	// console.log(result)
+
+	return (
+		<main>
+			<p>You are looking at my root route!</p>
+			<p>This has been viewed {result.rows[0].views}</p>
+			<p>The time is {evenEasierToReadDate}</p>
+		</main>
+	);
 }
